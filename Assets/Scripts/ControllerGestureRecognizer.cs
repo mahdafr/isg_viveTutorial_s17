5e295566977333cdc5791134df0cc727fb810859
mahdafr <mmafravi@gmail.com>
2017-05-18 11:32:06 -0600

    WIP: gesture recognition (found unity asset...)

---
+++ Assets/Scripts/ControllerGestureRecognizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGestureRecognizer : MonoBehaviour {
+    private SteamVR_TrackedObject trackedObj;
+    private Vector3 hitPoint; //position where laser hits
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
